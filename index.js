7532981f5c1f6b9f5e99dd17f8c728bad26f13f1
Richard Lai <it.rclai@gmail.com>
2016-04-25 18:22:54 -0400

    Overwrite Characteristics write prototype
    This needs to be done because React Native has no global Buffer object and so the check will cause JS to break. Assumes that a Buffer will be passed into it. It does reduce overhead. If a non-Buffer object is passed to it, the React Native bridge will at least throw an error.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+var Characteristic = require('noble/lib/characteristic');
+
+Characteristic.prototype.write = function(data, withoutResponse, callback) {
+  if (callback) {
+    this.once('write', function() {
+      callback(null);
+    });
+  }
+
+  this._noble.write(
+    this._peripheralId,
+    this._serviceUuid,
+    this.uuid,
+    data,
+    withoutResponse
+  );
+};
+
 var noble = require('noble/with-bindings');
 var bindings = require('./bindings');
 module.exports = new noble(bindings);
