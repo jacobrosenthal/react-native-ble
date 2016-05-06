c1c6f846dde9b9cab2328500a372abf91de685d7
Esa <esa.riihinen@geniem.com>
2016-05-06 20:18:48 +0300

    stub android module added to the repo (no functionality yet)

---
+++ android/src/main/java/com/geniem/rnble/RNBLEModule.java
@@ -0,0 +1,43 @@
+package com.geniem.rnble;
+
+import android.content.Context;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+
+class RNBLEModule extends ReactContextBaseJavaModule {
+    private Context context;
+
+    public RNBLEModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.context = reactContext;
+    }
+
+    /**
+     * @return the name of this module. This will be the name used to {@code require()} this module
+     * from javascript.
+     */
+    @Override
+    public String getName() {
+        return "RNBLE";
+    }
+
+    @ReactMethod
+    public void getState() {
+        WritableMap params = Arguments.createMap();
+        params.putString("state","unknown");
+        sendEvent("ble.stateChange", params);
+    }
+
+    private void sendEvent(String eventName, WritableMap params) {
+        getReactApplicationContext()
+            .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+            .emit(eventName, params);
+    }
+}
---
+++ bindings.android.js
@@ -0,0 +1,35 @@
+var debug = require('debug')('android-bindings');
+
+var events = require('events');
+var util = require('util');
+
+var {
+  DeviceEventEmitter,
+  NativeModules: { RNBLE },
+} = require('react-native');
+
+var Buffer = require('buffer').Buffer;
+
+var NobleBindings = function() {
+  DeviceEventEmitter.addListener('ble.stateChange', this.onStateChange.bind(this));
+};
+
+util.inherits(NobleBindings, events.EventEmitter);
+
+
+NobleBindings.prototype.onStateChange = function(params) {
+  // 'unknown', 'resetting', 'unsupported', 'unauthorized', 'poweredOff', 'poweredOn'
+  debug('state change ' + params.state);
+  this.emit('stateChange', params.state);
+};
+
+var nobleBindings = new NobleBindings();
+
+nobleBindings.init = function() {
+  setTimeout(function() {
+    RNBLE.getState();
+  }, 1000);
+};// Exports
+
+module.exports = nobleBindings;
+
