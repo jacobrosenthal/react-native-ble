c551e84b854949a340d4d8c81c93ca4bdaedbe25
Esa <esa.riihinen@geniem.com>
2016-05-08 23:17:01 +0300

    BLE scan and scan record parsing added

--- android/src/main/java/com/geniem/rnble/RNBLEModule.java
+++ android/src/main/java/com/geniem/rnble/RNBLEModule.java
@@ -1,6 +1,35 @@
+/* 
+
+The MIT License (MIT)
+
+Copyright (c) 2016 Esa Riihinen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+*/
+
 package com.geniem.rnble;
 
 import android.content.Context;
+import android.os.Handler;
+import android.util.Log;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -17,14 +46,25 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
+import android.os.ParcelUuid;
+import java.util.List;
+import java.util.Arrays;
+
+
 
 class RNBLEModule extends ReactContextBaseJavaModule {
+    private static final String TAG = "RNBLEModule";
+
     private Context context;
     private BluetoothAdapter bluetoothAdapter;
     private BluetoothManager bluetoothManager;
+    private final ScanCallback callback = new ScanCallback(this);
+
 
     public RNBLEModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -59,6 +99,33 @@ class RNBLEModule extends ReactContextBaseJavaModule {
         sendEvent("ble.stateChange", params);
     }
 
+    @ReactMethod
+    public void startScanning(ReadableArray serviceUuids, Boolean allowDuplicates) {
+        // allowDuplicates can not currently be used in Android
+
+        // Filtering by custom UUID is broken in Android 4.3 and 4.4, see:
+        // http://stackoverflow.com/questions/18019161/startlescan-with-128-bit-uuids-doesnt-work-on-native-android-ble-implementation?noredirect=1#comment27879874_18019161
+        // see also. https://github.com/tdicola/BTLETest/blob/master/app/src/main/java/com/tonydicola/bletest/app/MainActivity.java
+
+
+        
+        /*
+        new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    bluetoothAdapter.stopLeScan(callback);
+                }
+        }, 10*1000);
+        */
+
+        bluetoothAdapter.startLeScan(callback);
+    }
+
+    @ReactMethod
+    public void stopScanning() {
+        bluetoothAdapter.stopLeScan(callback);
+    }
+
     private void sendEvent(String eventName, WritableMap params) {
         getReactApplicationContext()
             .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
@@ -79,4 +146,81 @@ class RNBLEModule extends ReactContextBaseJavaModule {
                 return "unknown";
         }
     }
+
+    // callback for scan results
+
+    private class ScanCallback implements BluetoothAdapter.LeScanCallback {
+        private RNBLEModule rnbleModule;
+
+        public ScanCallback(RNBLEModule rnbleModule) {
+            this.rnbleModule = rnbleModule;
+        }
+
+        @Override
+        public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+
+            WritableMap params = Arguments.createMap();
+            ScanRecord record = ScanRecord.parseFromBytes(scanRecord);
+            WritableMap advertisement = Arguments.createMap();
+
+
+            //add service uuids to advertisement map
+            WritableArray serviceUuids = Arguments.createArray();
+            List<ParcelUuid> uuids = record.getServiceUuids();
+            if(uuids != null){
+               for(ParcelUuid uuid : uuids){
+                    serviceUuids.pushString(uuid.toString());
+                }
+            }
+
+            advertisement.putArray("serviceUuids", serviceUuids);
+
+            //add serviceData array to advetisement map
+            WritableArray serviceData = Arguments.createArray();
+            WritableMap serviceDataMap = Arguments.createMap();
+
+
+            ParcelUuid uuid = record.getServiceDataUuid();
+            byte[] data = record.getServiceData();
+
+            if(uuid != null && data != null){
+                serviceDataMap.putString("uuid", uuid.toString());
+                serviceDataMap.putString("data", Arrays.toString(data));
+                serviceData.pushMap(serviceDataMap);
+            }
+
+            advertisement.putArray("serviceData", serviceData);
+
+            //add manufacturer data to advertisement map
+            byte[] manufacturerData = record.getManufacturerSpecificData();
+            advertisement.putString("manufacturerData", Arrays.toString(manufacturerData));
+
+            //add local name to advertisement map
+            advertisement.putString("localName", record.getDeviceName());
+
+            //add tx power level to advertisement map
+            advertisement.putInt("txPowerLevel", record.getTxPowerLevel());
+
+            params.putMap("advertisement", advertisement);
+
+            //add rssi to params
+            params.putInt("rssi", rssi);
+
+            // add id to params
+            params.putString("id", device.getAddress());
+
+            // add address to params
+            params.putString("address", device.getAddress());
+
+            // add address type to params
+            params.putString("addressType", "unknown");
+
+            //add connectable to params
+            int flags = record.getAdvertiseFlags();
+            params.putBoolean("connectable", (flags & 2) == 2); //TODO: double check this to ensure it is correct
+
+            Log.d(TAG, params.toString());
+            rnbleModule.sendEvent("ble.discover", params);
+        }
+    }    
 }
--- bindings.android.js
+++ bindings.android.js
@@ -12,6 +12,7 @@ var Buffer = require('buffer').Buffer;
 
 var NobleBindings = function() {
   DeviceEventEmitter.addListener('ble.stateChange', this.onStateChange.bind(this));
+  DeviceEventEmitter.addListener('ble.discover', this.onDiscover.bind(this));
 };
 
 util.inherits(NobleBindings, events.EventEmitter);
@@ -23,6 +24,22 @@ NobleBindings.prototype.onStateChange = function(params) {
   this.emit('stateChange', params.state);
 };
 
+NobleBindings.prototype.onDiscover = function({ id, address, addressType, advertisement, connectable, rssi }) {
+  /*
+  if (advertisement.manufacturerData) {
+    advertisement.manufacturerData = new Buffer(advertisement.manufacturerData, 'base64');
+  }
+
+  if (advertisement.serviceData) {
+    advertisement.serviceData = advertisement.serviceData.map(({ uuid, data }) => ({
+      uuid,
+      data: new Buffer(data, 'base64'),
+    }));
+  } */
+
+  this.emit('discover', id, address, addressType, connectable, advertisement, rssi);
+};
+
 var nobleBindings = new NobleBindings();
 
 nobleBindings.init = function() {
@@ -31,7 +48,17 @@ nobleBindings.init = function() {
   }, 1000);
 };
 
-// Exports
+nobleBindings.startScanning = function(serviceUuids, allowDuplicates) {
+  var duplicates = allowDuplicates || false;
+  RNBLE.startScanning(serviceUuids, duplicates);
+  this.emit('scanStart');
+};
 
+nobleBindings.stopScanning = function() {
+  RNBLE.stopScanning();
+  this.emit('scanStop');
+};
+
+// Exports
 module.exports = nobleBindings;
 
