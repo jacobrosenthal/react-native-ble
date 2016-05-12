6c98d4b64a0b4a82c342fc14c963675fc7da97c0
Esa <esa.riihinen@geniem.com>
2016-05-12 23:05:00 +0300

    Reimplemented scanning using Android API level 21

--- android/src/main/java/com/geniem/rnble/RNBLEModule.java
+++ android/src/main/java/com/geniem/rnble/RNBLEModule.java
@@ -40,6 +40,12 @@ import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothProfile;
+import android.bluetooth.le.ScanRecord;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -54,6 +60,7 @@ import com.facebook.react.modules.core.DeviceEventManagerModule;
 import android.os.ParcelUuid;
 import java.util.List;
 import java.util.Arrays;
+import java.util.ArrayList;
 
 
 
@@ -63,7 +70,8 @@ class RNBLEModule extends ReactContextBaseJavaModule {
     private Context context;
     private BluetoothAdapter bluetoothAdapter;
     private BluetoothManager bluetoothManager;
-    private final ScanCallback callback = new ScanCallback(this);
+    private BluetoothLeScanner bluetoothLeScanner;
+    private ScanCallback scanCallback;
 
 
     public RNBLEModule(ReactApplicationContext reactContext) {
@@ -77,6 +85,7 @@ class RNBLEModule extends ReactContextBaseJavaModule {
         super.initialize();
         bluetoothManager = (BluetoothManager) this.context.getSystemService(ReactApplicationContext.BLUETOOTH_SERVICE);
         bluetoothAdapter = bluetoothManager.getAdapter();
+        bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
     }
 
     /**
@@ -103,19 +112,19 @@ class RNBLEModule extends ReactContextBaseJavaModule {
     public void startScanning(ReadableArray serviceUuids, Boolean allowDuplicates) {
         // allowDuplicates can not currently be used in Android
 
-        // Filtering by custom UUID is broken in Android 4.3 and 4.4, see:
-        // http://stackoverflow.com/questions/18019161/startlescan-with-128-bit-uuids-doesnt-work-on-native-android-ble-implementation?noredirect=1#comment27879874_18019161
-        // see also. https://github.com/tdicola/BTLETest/blob/master/app/src/main/java/com/tonydicola/bletest/app/MainActivity.java
-
-        if(bluetoothAdapter != null){
-            bluetoothAdapter.startLeScan(callback);
+        if(bluetoothLeScanner != null){
+            if (scanCallback == null) {
+                scanCallback = new RnbleScanCallback(this);
+                bluetoothLeScanner.startScan(buildScanFilters(), buildScanSettings(), scanCallback);
+            }
         }
     }
 
     @ReactMethod
     public void stopScanning() {
-        if(bluetoothAdapter != null){
-            bluetoothAdapter.stopLeScan(callback);
+        if(bluetoothLeScanner != null){
+            bluetoothLeScanner.stopScan(scanCallback);
+            scanCallback = null;
         }
     }
 
@@ -124,6 +133,23 @@ class RNBLEModule extends ReactContextBaseJavaModule {
             .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
             .emit(eventName, params);
     }
+
+    private List<ScanFilter> buildScanFilters() {
+        List<ScanFilter> scanFilters = new ArrayList<>();
+
+        ScanFilter.Builder builder = new ScanFilter.Builder();
+        // Comment out the below line to see all BLE devices around you
+        //builder.setServiceUuid("add service uuid");
+        scanFilters.add(builder.build());
+
+        return scanFilters;
+    }
+
+    private ScanSettings buildScanSettings() {
+        ScanSettings.Builder builder = new ScanSettings.Builder();
+        builder.setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY);
+        return builder.build();
+    }    
 
     private String stateToString(int state){
         switch (state) {
@@ -140,84 +166,112 @@ class RNBLEModule extends ReactContextBaseJavaModule {
         }
     }
 
-    // callback for scan results
+    //RnbleScanCallback scan callback
 
-    private class ScanCallback implements BluetoothAdapter.LeScanCallback {
+    private class RnbleScanCallback extends ScanCallback {
         private RNBLEModule rnbleModule;
 
-        public ScanCallback(RNBLEModule rnbleModule) {
+        public RnbleScanCallback(RNBLEModule rnbleModule) {
             this.rnbleModule = rnbleModule;
         }
 
+
         @Override
-        public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
-
-            WritableMap params = Arguments.createMap();
-            ScanRecord record = ScanRecord.parseFromBytes(scanRecord);
-            WritableMap advertisement = Arguments.createMap();
-
-
-            //add service uuids to advertisement map
-            WritableArray serviceUuids = Arguments.createArray();
-            List<ParcelUuid> uuids = record.getServiceUuids();
-            if(uuids != null){
-               for(ParcelUuid uuid : uuids){
-                    serviceUuids.pushString(uuid.toString());
-                }
+        public void onBatchScanResults(List<ScanResult> results) {
+            super.onBatchScanResults(results);
+            for (ScanResult result : results) {
+                processScanResult(result);
             }
-
-            advertisement.putArray("serviceUuids", serviceUuids);
-
-            //add serviceData array to advetisement map
-            WritableArray serviceData = Arguments.createArray();
-            WritableMap serviceDataMap = Arguments.createMap();
-
-
-            ParcelUuid uuid = record.getServiceDataUuid();
-            byte[] data = record.getServiceData();
-
-            if(uuid != null && data != null){
-                serviceDataMap.putString("uuid", uuid.toString());
-                serviceDataMap.putString("data", Arrays.toString(data));
-                serviceData.pushMap(serviceDataMap);
-            }
-
-            advertisement.putArray("serviceData", serviceData);
-
-            //add manufacturer data to advertisement map
-            byte[] manufacturerData = record.getManufacturerSpecificData();
-            if(manufacturerData != null){
-                advertisement.putString("manufacturerData", Arrays.toString(manufacturerData));
-            } else {
-                advertisement.putNull("manufacturerData");
-            }
-
-            //add local name to advertisement map
-            advertisement.putString("localName", record.getDeviceName());
-
-            //add tx power level to advertisement map
-            advertisement.putInt("txPowerLevel", record.getTxPowerLevel());
-
-            params.putMap("advertisement", advertisement);
-
-            //add rssi to params
-            params.putInt("rssi", rssi);
-
-            // add id to params
-            params.putString("id", device.getAddress());
-
-            // add address to params
-            params.putString("address", device.getAddress());
-
-            // add address type to params
-            params.putString("addressType", "unknown");
-
-            //add connectable to params
-            int flags = record.getAdvertiseFlags();
-            params.putBoolean("connectable", (flags & 2) == 2); //TODO: double check this to ensure it is correct
-
-            Log.d(TAG, params.toString());
-            rnbleModule.sendEvent("ble.discover", params);
         }
-    }    
+
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+            processScanResult(result);
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            super.onScanFailed(errorCode);
+            Log.d(TAG, "Scan failed with error: " + errorCode);
+        }
+
+        private void processScanResult(ScanResult scanResult) {
+
+            if(scanResult == null) {return;}
+
+            ScanRecord record = scanResult.getScanRecord();
+            BluetoothDevice device = scanResult.getDevice();
+
+            if(record != null){
+                WritableMap params = Arguments.createMap();
+                WritableMap advertisement = Arguments.createMap();
+
+                //add service uuids to advertisement map
+                WritableArray serviceUuids = Arguments.createArray();
+                List<ParcelUuid> uuids = record.getServiceUuids();
+                if(uuids != null){
+                   for(ParcelUuid uuid : uuids){
+                        serviceUuids.pushString(uuid.toString());
+                    }
+                }
+    
+                advertisement.putArray("serviceUuids", serviceUuids);
+
+                //add serviceData array to advetisement map
+                WritableArray serviceData = Arguments.createArray();
+                WritableMap serviceDataMap = Arguments.createMap();
+
+                if(uuids != null) {
+                    for(ParcelUuid uuid : uuids){
+                        byte[] data = record.getServiceData(uuid);
+                        if(uuid != null && data != null){
+                            serviceDataMap.putString("uuid", uuid.toString());
+                            serviceDataMap.putString("data", Arrays.toString(data));
+                            serviceData.pushMap(serviceDataMap);
+                        }
+                    }
+                }
+                advertisement.putArray("serviceData", serviceData);
+
+                //add manufacturer data to advertisement map
+                byte[] manufacturerData = null;
+                if(record.getManufacturerSpecificData() != null){
+                    manufacturerData = record.getManufacturerSpecificData().valueAt(0);
+                }
+                if(manufacturerData != null){
+                    advertisement.putString("manufacturerData", Arrays.toString(manufacturerData));
+                } else {
+                    advertisement.putNull("manufacturerData");
+                }
+
+                //add local name to advertisement map
+                advertisement.putString("localName", record.getDeviceName());
+
+                //add tx power level to advertisement map
+                advertisement.putInt("txPowerLevel", record.getTxPowerLevel());
+
+                params.putMap("advertisement", advertisement);
+
+                //add rssi to params
+                params.putInt("rssi", scanResult.getRssi());
+
+                // add id to params
+                params.putString("id", device.getAddress());
+
+                // add address to params
+                params.putString("address", device.getAddress());
+
+                // add address type to params
+                params.putString("addressType", "unknown");
+
+                //add connectable to params
+                int flags = record.getAdvertiseFlags();
+                params.putBoolean("connectable", (flags & 2) == 2); //TODO: double check this to ensure it is correct
+
+                Log.d(TAG, params.toString());
+                rnbleModule.sendEvent("ble.discover", params);
+            }
+        }
+    } 
 }
