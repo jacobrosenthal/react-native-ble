9d50ddf525b507e42269148f7c84ec52f9d8fe2a
Maxime Junger <maxime.junger@epitech.eu>
2016-05-23 16:13:33 +0200

    Changed setup() to getState() methot

---
+++ android/src/main/java/com/maximejunger/react/RNBLEModule.java
@@ -0,0 +1,392 @@
+/**
+* @Author: Maxime JUNGER <junger_m>
+* @Date:   18-04-2016
+* @Email:  maximejunger@gmail.com
+* @Last modified by:   junger_m
+* @Last modified time: 18-04-2016
+*/
+
+
+
+package com.maximejunger.react;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothHeadset;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.ParcelUuid;
+import android.os.Parcelable;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Logger;
+
+import javax.annotation.Nullable;
+
+/**
+ * Project - android - RNBLE
+ * Created by Maxime JUNGER - junger_m on 18/04/16.
+ * Email : maxime.junger@epitech.eu
+ */
+
+public class RNBLEModule extends ReactContextBaseJavaModule implements BluetoothAdapter.LeScanCallback {
+
+    private Map<String, BluetoothDevice> mPeripherals;
+    private BluetoothAdapter mBluetoothAdapter;
+    private BroadcastReceiver mReceiver;
+    private BluetoothGattCallbackHandler mBluetoothGattCallbackHandler;
+
+    private static final UUID CLIENT_CHARACTERISTIC_CONFIG_DESCRIPTOR_UUID = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
+
+    public RNBLEModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "RNBLE";
+    }
+
+    // use this as an inner class like here or as a top-level class
+//    public class MyBroadCastReceiver extends BroadcastReceiver {
+//
+//        ReactApplicationContext mReactApplicationContext;
+//
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//            // do something
+//            String action = intent.getAction();
+//
+//            if (BluetoothAdapter.ACTION_STATE_CHANGED.equals(action)) {
+//
+//                int extraCode = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1);
+//                WritableMap params = Arguments.createMap();
+//
+//                switch (extraCode) {
+//                    case BluetoothAdapter.STATE_OFF:
+//                        params.putString("state", "poweredOff");
+//                        break;
+//                    case BluetoothAdapter.STATE_ON:
+//                        params.putString("state", "poweredOn");
+//                        break;
+//                }
+//
+//
+//                if (params.hasKey("state") && params.getString("state") != null) {
+//                    sendEvent(this.mReactApplicationContext, "stateChange", params);
+//                }
+//            } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+//                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+//                // Create a new device item
+//                int i = 0;
+//                System.out.print(device.getName());
+//
+//                ParcelUuid[] pcl = device.getUuids();
+//
+//                System.out.println(device.getAddress());
+//
+//                System.out.println(device.getType());
+//                // DeviceItem newDevice = new DeviceItem(device.getName(), device.getAddress(), "false");
+//                // Add it to our adapter
+//                // mAdapter.add(newDevice);
+//            } else if (BluetoothDevice.ACTION_UUID.equals(action)) {
+//                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+//                Parcelable[] uuids = intent.getParcelableArrayExtra(BluetoothDevice.EXTRA_UUID);
+//                System.out.print("JE SUIS LA EHEHHEHE");
+//                for (Parcelable ep : uuids) {
+//                    System.out.print("UUID Records : " + ep.toString());
+//                    //Utilities.print("UUID records : "+ ep.toString());
+//                }
+//            }
+//        }
+//
+//        public MyBroadCastReceiver(ReactApplicationContext reactApplicationContext) {
+//            mReactApplicationContext = reactApplicationContext;
+//        }
+//    }
+
+    /**
+     * Send events to Javascript
+     *
+     * @param reactContext context of react
+     * @param eventName    name of event that Javascript is listening
+     * @param params       WritableMap of params
+     */
+    private void sendEvent(ReactContext reactContext,
+                           String eventName,
+                           @Nullable WritableMap params) {
+        reactContext
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit(eventName, params);
+    }
+
+    /**
+     * Prepare the Bluetooth and set the stateChange handler
+     * Send event of the current Bluetooth state to React
+     */
+    @ReactMethod
+    public void getState() {
+
+        mPeripherals = new HashMap<String, BluetoothDevice>();
+
+        BluetoothManager manager = (BluetoothManager) this.getReactApplicationContext().getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = manager.getAdapter();
+
+        WritableMap params = Arguments.createMap();
+
+        if (mBluetoothAdapter == null || !this.getReactApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            params.putString("state", "unsupported");
+        } else {
+            if (!mBluetoothAdapter.isEnabled()) {
+                params.putString("state", "poweredOff");
+            } else {
+                params.putString("state", "poweredOn");
+            }
+        }
+
+        sendEvent(this.getReactApplicationContext(), "ble.stateChange", params);
+
+//        this.mReceiver = new MyBroadCastReceiver(this.getReactApplicationContext());
+//        this.getReactApplicationContext().registerReceiver(this.mReceiver, new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
+    }
+
+    /**
+     * Start scanning for needed uuids
+     * @param uuids UUIDS needed
+     * @param allowDuplicates true/false
+     */
+    @ReactMethod
+    public void startScanning(ReadableArray uuids, Boolean allowDuplicates) {
+
+        this.mPeripherals.clear();
+
+        UUID[] arrayUUIDS = new UUID[uuids.size()];
+
+        int y = 0;
+        y += 1;
+
+        for (int i = 0; i < uuids.size(); i++) {
+            arrayUUIDS[i] =  BluetoothUUIDHelper.shortUUIDToLong(uuids.getString(i));
+        }
+
+        this.mBluetoothAdapter.startLeScan(arrayUUIDS, this);
+    }
+
+    /**
+     * Called when a device is found
+     * @param device device found
+     * @param rssi Signal strength
+     * @param scanRecord record
+     */
+    @Override
+    public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+
+        WritableMap params = Arguments.createMap();
+
+        /**
+         * Save devices in map if not present
+         */
+        if (!this.mPeripherals.containsKey(device.getAddress()))
+            this.mPeripherals.put(device.getAddress(), device);
+
+        params.putString("name", device.getName());
+        params.putString("address", device.getAddress());
+        params.putInt("rssi", rssi);
+
+        sendEvent(this.getReactApplicationContext(), "discover", params);
+    }
+
+    /**
+     * Connect to device
+     * @param address of the device
+     */
+    @ReactMethod
+    public void connect(String address) {
+
+        BluetoothDevice device = this.mPeripherals.get(address);
+        WritableMap params = Arguments.createMap();
+
+        if (mBluetoothGattCallbackHandler != null && mBluetoothGattCallbackHandler.getmBluetoothGatt() != null) {
+            mBluetoothGattCallbackHandler.getmBluetoothGatt().close();
+            mBluetoothGattCallbackHandler = null;
+        }
+
+        if (device != null) {
+            mBluetoothGattCallbackHandler = new BluetoothGattCallbackHandler(this.getReactApplicationContext());
+            device.connectGatt(this.getReactApplicationContext(), true, mBluetoothGattCallbackHandler);
+        } else {
+            params.putString("error", "Device not found");
+            sendEvent(this.getReactApplicationContext(), "connect", params);
+        }
+
+    }
+
+    @ReactMethod
+    public void disconnect(String address) {
+        BluetoothDevice device = this.mPeripherals.get(address);
+        WritableMap params = Arguments.createMap();
+
+        if (device != null) {
+            if (this.mBluetoothGattCallbackHandler != null) {
+                this.mBluetoothGattCallbackHandler.getmBluetoothGatt().close();
+                params.putString("address", device.getAddress());
+                sendEvent(this.getReactApplicationContext(), "disconnect", params);
+            }
+        }
+    }
+
+    /**
+     * Discover services of device
+     * @param address of the device
+     * @param uuids wanted
+     */
+    @ReactMethod
+    public void discoverServices(String address, ReadableArray uuids) {
+
+        BluetoothDevice device = this.mPeripherals.get(address);
+        WritableMap params = Arguments.createMap();
+
+        if (device != null && this.mBluetoothGattCallbackHandler.getmBluetoothGatt().getDevice().getAddress().equals(device.getAddress())) {
+            this.mBluetoothGattCallbackHandler.getmBluetoothGatt().discoverServices();
+        } else {
+            params.putString("error", "Device not found");
+            sendEvent(this.getReactApplicationContext(), "services", params);
+        }
+    }
+
+    /**
+     * Discover Characteristics for service
+     * @param address of device
+     * @param serviceUUID UUID of service where characteristics are
+     * @param characteristicsUUIDs Characteristics wanted
+     */
+    @ReactMethod
+    public void discoverCharacteristicsForService(String address, String serviceUUID, ReadableArray characteristicsUUIDs) {
+
+        BluetoothDevice device = this.mPeripherals.get(address);
+        WritableMap params = Arguments.createMap();
+        WritableArray characteristics = Arguments.createArray();
+        if (device != null && this.mBluetoothGattCallbackHandler.getmBluetoothGatt().getDevice().getAddress().equals(device.getAddress())) {
+
+            BluetoothGattService service = mBluetoothGattCallbackHandler.getmBluetoothGatt().getService(BluetoothUUIDHelper.shortUUIDToLong(serviceUUID));
+
+            if (service != null && BluetoothUUIDHelper.longUUIDToShort(String.valueOf(service.getUuid())).equals(serviceUUID)) {
+
+                params.putString("address", address);
+                params.putString("serviceUUID", serviceUUID);
+
+                if (characteristicsUUIDs.size() == 0) {
+                    List<BluetoothGattCharacteristic> characs = service.getCharacteristics();
+
+                    for (BluetoothGattCharacteristic blc : characs) {
+                        String uuidStr = BluetoothUUIDHelper.longUUIDToShort(blc.getUuid().toString()).toUpperCase();
+                        characteristics.pushString(uuidStr);
+                    }
+
+                } else {
+                    for (int i = 0; i < characteristicsUUIDs.size(); i++) {
+                        UUID uuid = BluetoothUUIDHelper.shortUUIDToLong(characteristicsUUIDs.getString(i));
+                        String uuidStr = BluetoothUUIDHelper.longUUIDToShort(service.getCharacteristic(uuid).getUuid().toString()).toUpperCase();
+                        characteristics.pushString(uuidStr);
+                    }
+                }
+                params.putArray("characteristics", characteristics);
+
+                sendEvent(this.getReactApplicationContext(), "characteristics", params);
+            } else {
+                params.putString("error", "Device not found");
+                sendEvent(this.getReactApplicationContext(), "characteristics", params);
+            }
+        }
+    }
+
+    /**
+     * Read value of characteristic
+     * @param address of the device
+     * @param serviceUUID UUID of service where characteristic is
+     * @param characteristicUUID Characteristic we want to read
+     */
+    @ReactMethod
+    public void readCharacteristic(String address, String serviceUUID, String characteristicUUID) {
+
+        BluetoothGattCharacteristic charac = this.mBluetoothGattCallbackHandler.getmBluetoothGatt().getService(BluetoothUUIDHelper.shortUUIDToLong(serviceUUID)).getCharacteristic(BluetoothUUIDHelper.shortUUIDToLong(characteristicUUID));
+        this.mBluetoothGattCallbackHandler.getmBluetoothGatt().readCharacteristic(charac);
+
+    }
+
+    @ReactMethod
+    public void subscribeCharacteristic(String address, String serviceUUID, String characteristicUUID) {
+
+        WritableMap params = Arguments.createMap();
+
+        BluetoothDevice device = this.mPeripherals.get(address);
+
+        if (device == null) {
+            params.putString("error", "Device not found");
+            return;
+        }
+
+        BluetoothGattService service = this.mBluetoothGattCallbackHandler.getmBluetoothGatt().getService(BluetoothUUIDHelper.shortUUIDToLong(serviceUUID));
+
+        if (service == null) {
+            params.putString("error", "Service not found");
+            return;
+        }
+
+        BluetoothGattCharacteristic characteristic = service.getCharacteristic(BluetoothUUIDHelper.shortUUIDToLong(characteristicUUID));
+
+        if (characteristic == null) {
+            params.putString("error", "Characteristic not found");
+            return;
+        }
+
+
+        // Check characteristic property
+        final int properties = characteristic.getProperties();
+        if ((properties & BluetoothGattCharacteristic.PROPERTY_NOTIFY) == 0)
+            return;
+
+        this.mBluetoothGattCallbackHandler.getmBluetoothGatt().setCharacteristicNotification(characteristic, true);
+
+        final BluetoothGattDescriptor descriptor = characteristic.getDescriptor(CLIENT_CHARACTERISTIC_CONFIG_DESCRIPTOR_UUID);
+
+        if (descriptor != null) {
+            descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+            this.mBluetoothGattCallbackHandler.getmBluetoothGatt().writeDescriptor(descriptor);
+        }
+    }
+
+    @ReactMethod
+    public void test() {
+        Log.i("TEST", "TEST HELLO");
+    }
+
+}
