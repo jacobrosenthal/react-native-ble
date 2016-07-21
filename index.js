91a08bf64b62882d13b6210dc3d1ac5cfb2c425d
Jacob Rosenthal <jakerosenthal@gmail.com>
2016-07-21 01:50:50 -0700

    remove descriptor and characteristic overload

--- index.js
+++ index.js
@@ -1,38 +1,3 @@
-var Characteristic = require('noble/lib/characteristic');
-
-Characteristic.prototype.write = function(data, withoutResponse, callback) {
-  if (callback) {
-    this.once('write', function() {
-      callback(null);
-    });
-  }
-
-  this._noble.write(
-    this._peripheralId,
-    this._serviceUuid,
-    this.uuid,
-    data,
-    withoutResponse
-  );
-};
-
-var Descriptor = require('noble/lib/descriptor');
-
-Descriptor.prototype.writeValue = function(data, callback) {
-  if (callback) {
-    this.once('valueWrite', function() {
-      callback(null);
-    });
-  }
-  this._noble.writeValue(
-    this._peripheralId,
-    this._serviceUuid,
-    this._characteristicUuid,
-    this.uuid,
-    data
-  );
-};
-
 var noble = require('noble/with-bindings');
 var bindings = require('./bindings');
 module.exports = new noble(bindings);
