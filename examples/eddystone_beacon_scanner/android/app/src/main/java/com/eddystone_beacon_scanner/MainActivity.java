e8a80fa71bc53e30dcf6a6ea48ba766b028a4e4f
Jacob Rosenthal <jakerosenthal@gmail.com>
2016-09-21 09:54:08 -0400

    generate new example

---
+++ examples/eddystone_beacon_scanner/android/app/src/main/java/com/eddystone_beacon_scanner/MainActivity.java
@@ -0,0 +1,15 @@
+package com.eddystone_beacon_scanner;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "eddystone_beacon_scanner";
+    }
+}
