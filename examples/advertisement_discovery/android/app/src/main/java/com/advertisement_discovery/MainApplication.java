4a9ac3793c8ac9cccda4b9ed37f0b63ae4e3d800
Jacob Rosenthal <jakerosenthal@gmail.com>
2016-09-23 18:56:20 -0400

    for some reason rnpm link isnt enough, need to edit MainApplication.java

--- examples/advertisement_discovery/android/app/src/main/java/com/advertisement_discovery/MainApplication.java
+++ examples/advertisement_discovery/android/app/src/main/java/com/advertisement_discovery/MainApplication.java
@@ -8,7 +8,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-
+import com.geniem.rnble.RNBLEPackage;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,7 +23,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RNBLEPackage()
       );
     }
   };
